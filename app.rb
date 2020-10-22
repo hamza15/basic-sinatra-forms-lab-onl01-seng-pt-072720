54c20c8dcbb4a4f83be96ebc64b1a295a5357736
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-22 06:27:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '' do
+    @team_name = params[:team_name]
+    erb :team
+  end
 
 end
